e3643175e04a1970c9b767b7e2bbabc22f6cdca8
Tomi Leppänen <tomppeli12@gmail.com>
2012-05-12 10:26:58 +0300

    Added many new bonuses (currently 14 different bonuses) and made some fixes
    Fixed for loop in checkForBonus
    Now following bonuses are supported:
      * immortalize
      * narrow
      * slowdown
      * speedup
      * warp
      * widen
      * mirrorKeys
      * narrowOthers
      * slowdownOthers
      * speedupOthers
      * warpAll
      * widenOthers
      * clear
      * immortalizeAll

--- bonuses.js
+++ bonuses.js
@@ -26,26 +26,72 @@ var BONUS_R_POW2 = m.pow(BONUS_R,2); // Optimization
 
 /* Creates bonus and adds it to gamearea */
 /*
- * Valid types are:
- *  - widen
- *  - narrow
- *  - immortalize
- *  - others to be added
+ * Valid types and their circle colours are (stroke,fill):
+ *  - immortalize (green,green)
+ *  - narrow (green,lighter green)
+ *  - slowdown (green, darker blue)
+ *  - speedup (green,red)
+ *  - warp (green,white)
+ *  - widen (green,lighter blue)
+ *  - mirrorKeys (red, black)
+ *  - narrowOthers (red,lighter green)
+ *  - slowdownOthers (red,darker blue)
+ *  - speedupOthers (red,red)
+ *  - warpAll (blue,white)
+ *  - widenOthers (red,lighher blue)
+ *  - clear (blue,black)
+ *  - immortalizeAll (blue,green)
  */
 function bonus(type,x,y) {
-    this.circle = document.createElementNS(NS,"circle");
     this.x = x;
     this.y = y;
     this.type = type;
     this.remove = removeBonus;
+    this.circle = document.createElementNS(NS,"circle");
     this.circle = elementSetAttributes(this.circle,{"cx":this.x, "cy":this.y, 
-        "r":BONUS_R, "id":"bonus_"+x+"_"+y});
-    if (type == "widen") {
-        this.circle = elementSetAttributes(this.circle, {"fill":"blue"});
+        "r":BONUS_R, "stroke-width":3, "id":"bonus_"+x+"_"+y});
+    if (type == "immortalize") {
+        this.circle = elementSetAttributes(this.circle, {"stroke":"green", 
+            "fill":"00CC00"});
     } else if (type == "narrow") {
-        this.circle = elementSetAttributes(this.circle, {"fill":"green"});
-    } else if (type == "immortalize") {
-        this.circle = elementSetAttributes(this.circle, {"fill":"red"});
+        this.circle = elementSetAttributes(this.circle, {"stroke":"green", 
+            "fill":"00FF33"});
+    } else if (type == "slowdown") {
+        this.circle = elementSetAttributes(this.circle, {"stroke":"green", 
+            "fill":"3300FF"});
+    } else if (type == "speedup") {
+        this.circle = elementSetAttributes(this.circle, {"stroke":"green", 
+            "fill":"FF3300"});
+    } else if (type == "warp") {
+        this.circle = elementSetAttributes(this.circle, {"stroke":"green", 
+            "fill":"FFFFFF"});
+    } else if (type == "widen") {
+        this.circle = elementSetAttributes(this.circle, {"stroke":"green", 
+            "fill":"00CCFF"});
+    } else if (type == "mirrorKeys") {
+        this.circle = elementSetAttributes(this.circle, {"stroke":"blue", 
+            "fill":"000000"});
+    } else if (type == "narrowOthers") {
+        this.circle = elementSetAttributes(this.circle, {"stroke":"green", 
+            "fill":"00FF33"});
+    } else if (type == "slowdownOthers") {
+        this.circle = elementSetAttributes(this.circle, {"stroke":"red", 
+            "fill":"3300FF"});
+    } else if (type == "speedupOthers") {
+        this.circle = elementSetAttributes(this.circle, {"stroke":"red", 
+            "fill":"FF3300"});
+    } else if (type == "warpAll") {
+        this.circle = elementSetAttributes(this.circle, {"stroke":"blue", 
+            "fill":"FFFFFF"});
+    } else if (type == "widenOthers") {
+        this.circle = elementSetAttributes(this.circle, {"stroke":"green", 
+            "fill":"00CCFF"});
+    } else if (type == "clear") {
+        this.circle = elementSetAttributes(this.circle, {"stroke":"blue", 
+            "fill":"000000"});
+    } else if (type == "immortalizeAll") {
+        this.circle = elementSetAttributes(this.circle, {"stroke":"blue", 
+            "fill":"00CC00"});
     }
     gamearea.appendChild(this.circle);
 }
@@ -59,19 +105,30 @@ function removeBonus() {
 function addBonus() {
     var x = m.floor(m.random()*(game_width-100)+50);
     var y = m.floor(m.random()*(game_height-100)+50);
-    var type = m.floor(m.random()*3)
+    var type = m.ceil(m.random()*14);
     switch(type) {
-        case 1: type = "widen"; break;
+        case 1: type = "immortalize"; break;
         case 2: type = "narrow"; break;
-        default: type = "immortalize"; break;
+        case 3: type = "slowdown"; break;
+        case 4: type = "speedup"; break;
+        case 5: type = "warp"; break;
+        case 6: type = "widen"; break;
+        case 7: type = "mirrorKeys"; break;
+        case 8: type = "narrowOthers"; break;
+        case 9: type = "slowdownOthers"; break;
+        case 10: type = "speedupOthers"; break;
+        case 11: type = "warpAll"; break;
+        case 12: type = "widenOthers"; break;
+        case 13: type = "clear"; break;
+        case 14: type = "immortalizeAll"; break;
     }
     bonuses.push(new bonus(type,x,y));
 }
 
 function checkForBonus(x,y) {
     // Add code to check if player happened to hit a bonus
-    for (z in bonuses) {
-        var distance = m.pow((x-bonuses[z].x),2)+m.pow((y-bonuses[z].y),2);
-        if (distance < BONUS_R_POW2) return bonuses[z];
+    for (var i in bonuses) {
+        var distance = m.pow((x-bonuses[i].x),2)+m.pow((y-bonuses[i].y),2);
+        if (distance < BONUS_R_POW2) return bonuses[i];
     } return false;
 }
